eec39a5dd64fb9cf2d14411caa80eefdec382892
Ben Albritton <benalbritton@outlook.com>
2017-10-13 00:00:27 -0500

    Added code to satisfy unit test

--- Game/checkWinLogic.py
+++ Game/checkWinLogic.py
@@ -6,22 +6,52 @@ class WinLogic:
 		 return
 		 
 	def ClearMatrix(self):
-		pass
+		if WinLogic.debug:
+			print("Clearing Matrix")
+		WinLogic.matrix = [[0 for x in range(3)] for y in range(3)]
 	
 	def SetPlayer(self, player):
-		pass
+		if WinLogic.debug:
+			print ("Setting player as: " + player)
+		WinLogic.player = player
 		
 	def GetPlayer(self):
-		pass
+		if WinLogic.debug:
+			print ("Returning: " + WinLogic.player)
+		return WinLogic.player
 	
 	def SetGrid(self, val, w, h):
-		pass
+		if WinLogic.debug:
+			print ("Setting grid...")
+		WinLogic.matrix[w][h] = val
 		
 	def GetGrid(self, w, h):
-		pass
+		return WinLogic.matrix[w] [h]
 		
 	def hasPlayerWon(self, player):
-		pass
+		if WinLogic.debug:
+			print ("Checking for winner")
+		if((WinLogic.matrix[0] [0] == WinLogic.player)and(WinLogic.matrix[1] [0] == WinLogic.player)and(WinLogic.matrix[2] [0] == WinLogic.player)):
+			return True
+		elif((WinLogic.matrix[0] [1] == WinLogic.player)and(WinLogic.matrix[1] [1] == WinLogic.player)and(WinLogic.matrix[2] [1] == WinLogic.player)):
+			return True
+		elif((WinLogic.matrix[0] [2] == WinLogic.player)and(WinLogic.matrix[1] [2] == WinLogic.player)and(WinLogic.matrix[2] [2] == WinLogic.player)):
+			return True
+		if((WinLogic.matrix[0] [0] == WinLogic.player)and(WinLogic.matrix[0] [1] == WinLogic.player)and(WinLogic.matrix[0] [2] == WinLogic.player)):
+			return True
+		if((WinLogic.matrix[1] [0] == WinLogic.player)and(WinLogic.matrix[1] [1] == WinLogic.player)and(WinLogic.matrix[1] [2] == WinLogic.player)):
+			return True
+		if((WinLogic.matrix[2] [0] == WinLogic.player)and(WinLogic.matrix[2] [1] == WinLogic.player)and(WinLogic.matrix[2] [2] == WinLogic.player)):
+			return True
+		if((WinLogic.matrix[0] [0] == WinLogic.player)and(WinLogic.matrix[1] [1] == WinLogic.player)and(WinLogic.matrix[2] [2] == WinLogic.player)):
+			return True
+		if((WinLogic.matrix[2] [0] == WinLogic.player)and(WinLogic.matrix[1] [1] == WinLogic.player)and(WinLogic.matrix[0] [2] == WinLogic.player)):
+			return True
+		
+		else:
+			if WinLogic.debug:
+				print ("No Player has won")
+			return False
 			
 
 m = WinLogic()
