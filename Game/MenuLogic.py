29ab4969d72ecafea062196287712a5a179506c1
McKenzie Hafkemeyer <mckenzie.hafkemeyer@gmail.com>
2017-10-16 23:13:34 -0500

    Empty MenuLogic class

---
+++ Game/MenuLogic.py
@@ -0,0 +1,12 @@
+class MenuLogic:
+    player1 = ""
+    player2 = ""
+
+    def __init__(self):
+        pass
+
+    def setPlayerName(self, playerNumber, playerName):
+        pass
+
+    def launchGame(self, playerName1, playerName2):
+        pass
