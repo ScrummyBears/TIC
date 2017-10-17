c5b414b04bc898c11a98bbd68fd956dd6f46c855
McKenzie Hafkemeyer <mckenzie.hafkemeyer@gmail.com>
2017-10-16 23:17:30 -0500

    Failing unit test for MenuLogic class

---
+++ unittests/MenuLogic_Unittest.py
@@ -0,0 +1,29 @@
+#This can run independently
+import unittest, sys, os, unittest.mock
+
+#modifying sys path to import MenuLogic
+lib_path = os.path.abspath(os.path.join('..','Game'))
+sys.path.append(lib_path)
+
+# noinspection PyUnresolvedReferences
+from MenuLogic import MenuLogic
+
+class CheckMenuLogic(unittest.TestCase):
+    def test_setPlayerName(self):
+        menu = MenuLogic()
+        menu.setPlayerName(1, "")
+        menu.setPlayerName(2, "John Doe")
+
+        self.assertEqual(menu.player1, "Player 1")
+        self.assertEqual(menu.player2, "John Doe")
+
+    def test_launchGame(self):
+        menu = MenuLogic()
+        menu.launchGame(" ", "John Doe")
+
+        self.assertEqual(menu.player1, "Player 1")
+        self.assertEqual(menu.player2, "John Doe")
+
+#unittest.main()
+if __name__ == '__main__':
+    unittest.main()
