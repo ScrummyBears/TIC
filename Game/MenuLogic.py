b60811b29a88f323db7f484402c8d426eac51494
McKenzie Hafkemeyer <mckenzie.hafkemeyer@gmail.com>
2017-10-19 18:53:15 -0500

    Check in MenuLogic implementation

--- Game/MenuLogic.py
+++ Game/MenuLogic.py
@@ -3,10 +3,22 @@ class MenuLogic:
     player2 = ""
 
     def __init__(self):
-        pass
+        self.player1 = ""
+        self.player2 = ""
 
     def setPlayerName(self, playerNumber, playerName):
-        pass
+        if playerNumber == 1:
+            if playerName.strip() == "":
+                self.player1 = "Player 1"
+            else:
+                self.player1 = playerName
+        if playerNumber == 2:
+            if playerName.strip() == "":
+                self.player2 = "Player 2"
+            else:
+                self.player2 = playerName
 
     def launchGame(self, playerName1, playerName2):
-        pass
+        self.setPlayerName(1, playerName1)
+        self.setPlayerName(2, playerName2)
+
