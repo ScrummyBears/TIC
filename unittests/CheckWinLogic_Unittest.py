a715ce1a34c73827b79dcc904887f41926a09f53
McKenzie Hafkemeyer <mckenzie.hafkemeyer@gmail.com>
2017-10-21 22:51:18 -0500

    Added additional unit tests for isCatsGame
    Added unit tests for when isCatsGame should return false (when there is a winner or the board isn't full)

--- unittests/CheckWinLogic_Unittest.py
+++ unittests/CheckWinLogic_Unittest.py
@@ -130,7 +130,7 @@ class CheckWinLogic(unittest.TestCase):
 		win.SetGrid("Player2",0,2)
 		self.assertFalse(win.hasPlayerWon(player)) #lose case 1
 
-	def test_isCatsGame(self):
+	def test_isCatsGame_CaseTrue(self):
 		win = WinLogic()
 		player1 = "Player1"
 		player2 = "Player2"
@@ -146,6 +146,38 @@ class CheckWinLogic(unittest.TestCase):
 		win.SetGrid(player2, 1, 2)
 		win.SetGrid(player2, 2, 1)
 		self.assertTrue(win.isCatsGame())
+
+	def test_isCatsGame_CaseFalse_BoardNotFull(self):
+		win = WinLogic()
+		player1 = "Player1"
+		player2 = "Player2"
+		win.SetPlayer(player1)
+		win.ClearMatrix()
+		win.SetGrid(player1, 2, 0)
+		win.SetGrid(player1, 2, 2)
+		win.SetGrid(player2, 0, 0)
+		win.SetGrid(player2, 1, 1)
+		win.SetGrid(player2, 1, 2)
+		win.SetGrid(player2, 2, 1)
+		self.assertFalse(win.isCatsGame())
+
+	def test_isCatsGame_CaseFalse_WinnerExists(self):
+		win = WinLogic()
+		player1 = "Player1"
+		player2 = "Player2"
+		win.SetPlayer(player1)
+		win.ClearMatrix()
+		win.SetGrid(player1, 0, 0)
+		win.SetGrid(player1, 1, 1)
+		win.SetGrid(player1, 2, 2)
+		win.SetGrid(player1, 2, 1)
+		win.SetGrid(player2, 0, 1)
+		win.SetGrid(player2, 0, 2)
+		win.SetGrid(player2, 1, 0)
+		win.SetGrid(player2, 1, 2)
+		win.SetGrid(player2, 2, 0)
+		self.assertFalse(win.isCatsGame())
+
 
 #unittest.main()
 if __name__ == '__main__':
