fc3bfea422b80fbb28f6c44f7ad736fc61c420b2
Ben Albritton <benalbritton@outlook.com>
2017-10-23 22:33:30 -0500

    Refactoring unittest

--- unittests/UnitTestDriver.py
+++ unittests/UnitTestDriver.py
@@ -1,28 +1,25 @@
 import unittest, os, sys
 
 #import the actual unit test
-import CheckWinLogic_Unittest
+import CheckWinLogic_Unittest, CheckCatsGame_Unittest, TurnManagement_Unittest, MenuLogic_Unittest
 
-print ("\n###################\nChecking CheckWinLogic...\n###################\n")
+
+#Loading the test individually
 Test_CheckWinLogic = unittest.TestLoader().loadTestsFromModule(CheckWinLogic_Unittest)
+Test_CheckCatsGame = unittest.TestLoader().loadTestsFromModule(CheckCatsGame_Unittest)
+Test_TurnManagement = unittest.TestLoader().loadTestsFromModule(TurnManagement_Unittest)
+Test_MenuLogic = unittest.TestLoader().loadTestsFromModule(MenuLogic_Unittest)
+
+#Run each test
+print ("\n###################\nChecking CheckWinLogic...\n###################\n")
 unittest.TextTestRunner(verbosity=2).run(Test_CheckWinLogic)
 
-import CheckCatsGame_Unittest
-
 print ("\n###################\nChecking CheckWinLogic - Cats Game...\n###################\n")
-Test_CheckCatsGame = unittest.TestLoader().loadTestsFromModule(CheckCatsGame_Unittest)
 unittest.TextTestRunner(verbosity=2).run(Test_CheckCatsGame)
 
-
-import TurnManagement_Unittest
-
 print ("\n###################\nChecking TurnManagement...\n###################\n")
-Test_TurnManagement = unittest.TestLoader().loadTestsFromModule(TurnManagement_Unittest)
 unittest.TextTestRunner(verbosity=2).run(Test_TurnManagement)
 
-import MenuLogic_Unittest
-
 print ("\n###################\nChecking MenuLogic...\n###################\n")
-Test_MenuLogic = unittest.TestLoader().loadTestsFromModule(MenuLogic_Unittest)
 unittest.TextTestRunner(verbosity=2).run(Test_MenuLogic)
 
