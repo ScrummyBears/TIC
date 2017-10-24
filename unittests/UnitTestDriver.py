4c848dca8dd2126db1f7561889717f1cf03466c6
Ben Albritton <benalbritton@outlook.com>
2017-10-23 20:00:19 -0500

    Updated framework to drive unittest

--- unittests/UnitTestDriver.py
+++ unittests/UnitTestDriver.py
@@ -7,3 +7,8 @@ print ("\n###################\nChecking CheckWinLogic...\n###################\n"
 Test_CheckWinLogic = unittest.TestLoader().loadTestsFromModule(CheckWinLogic_Unittest)
 unittest.TextTestRunner(verbosity=2).run(Test_CheckWinLogic)
 
+import TurnManagement_Unittest
+
+print ("\n###################\nChecking TurnManagement...\n###################\n")
+Test_TurnManagement = unittest.TestLoader().loadTestsFromModule(TurnManagement_Unittest)
+unittest.TextTestRunner(verbosity=2).run(Test_TurnManagement)
